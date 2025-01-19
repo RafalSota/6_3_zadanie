0737b908632a72d14c3a429bd16fddb08786693b
Rafal Sota <biuro@unico-group.pl>
2025-01-19 20:33:31 +0100

    data import from csv file, testing SQL commands

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sqlalchemy
+#import sqlalchemy
 from sqlalchemy import Table, Column, Integer, Float, String, Date, MetaData
 from sqlalchemy import create_engine
-from datetime import date
+import pandas as pd
 
-engine = create_engine('sqlite:///database.db')
+engine = create_engine('sqlite:///database.db', echo=False)
 meta = MetaData()
 
 stations = Table(
@@ -29,15 +29,32 @@ measure = Table(
 
 meta.create_all(engine)
 print(engine.table_names())
+conn = engine.connect()
 
+df = pd.read_csv('clean_stations.csv')
+print("HEAD pliku clean_stations.csv: ")
+print(df.head())
+for index, row in df.iterrows():
+   insert_data_csv_stations = stations.insert().values(station = row['station'], latitude = row['latitude'], longitude = row['longitude'], elevation = row['elevation'], name = row['name'], country = row['country'], state = row['state'])
+   print(f"Importuję pozycję nr: {index}")
+   conn.execute(insert_data_csv_stations)
+
+df = pd.read_csv('clean_measure.csv')
+print("HEAD pliku clean_measure.csv: ")
+print(df.head())
+for index, row in df.iterrows():
+   insert_data_csv_measure = measure.insert().values(station = row['station'], date = row['station'], precip = row['precip'], tobs = row['tobs'])
+   print(f"Importuję pozycję nr: {index}")
+   conn.execute(insert_data_csv_measure)
+conn.close()
 
 #Ręczne polecenie INSERT - testowanie poprawności utworzonej bazy danych
-###################################
+##################################
 #tworzenie polecenia SQL
-ins01_stations = stations.insert().values(station="USC00534397", latitude = 21.2716, longitude = -157.8168, elevation = 3.0, name = "WAIKIKI 717.2", country = "US", state = "HI")
+ins01_stations = stations.insert().values(station="_manual_USC00534397", latitude = 21.2716, longitude = -157.8168, elevation = 3.0, name = "WAIKIKI 717.2", country = "US", state = "HI")
 #print(ins01_stations) # wydruk polecenia w SQL
 #print(ins01_stations.compile().params)  # wydruk parametrów polecenia SQL 'ins'
-ins01_measure = measure.insert().values(station = "USC00534397", date = "2010-01-01", precip = 0.08, tobs = 65)
+ins01_measure = measure.insert().values(station = "manual_USC00534397", date = "2010-01-01", precip = 0.08, tobs = 65)
 conn = engine.connect()
 #result = conn.execute(ins01_stations)
 conn.execute(ins01_stations)
@@ -51,13 +68,25 @@ for r in results:
 s = measure.select()
 result = conn.execute(s)
 print("Measure: ")
-for r in result:
+count = 0
+for r in result:        
    print(r)
+   count += 1
+   if count == 20:   # ograniczenie ilości wyświetleń
+      break
 
 s = stations.select().where(stations.c.id < 4)
 result = conn.execute(s)
-print("Station, ID<2: ")
+print("Station, ID<4: ")
 for row in result:
    print(row)   
 
+s = measure.select().where(measure.c.id < 50)
+result = conn.execute(s)
+print("Odczyty, ID<50: ")
+for row in result:
+   print(row)   
 
+print("First 5 Stations: ")
+rows = conn.execute("SELECT * FROM stations LIMIT 5").fetchall()
+print(rows)
